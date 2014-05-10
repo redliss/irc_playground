3c273bca24e0b3c019b038d3d1a3e0cbc839ce76
Liss M <lissm@sdf.org>
2014-05-10 12:39:23 -0500

    WIP: capturing key info as params

--- application.rb
+++ application.rb
@@ -4,3 +4,7 @@ require 'sinatra'
 get '/' do
   "It's a thing! It's there! It's a thing!"
 end
+
+get '/users/:character_name/:description' do
+  "Character name: #{params[:character_name]}\n Description: #{params[:description]}"
+end
